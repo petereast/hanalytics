aafb9be085bb0e866aff1dce19acfbe4b3be04bf
Peter East <peter@repositive.io>
2019-04-03 11:09:12 +0100

    Add endpoint skeletons

--- hanalytics-agent/app/Main.hs
+++ hanalytics-agent/app/Main.hs
@@ -1,16 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Control.Applicative
+import Database.Redis (Connection, checkedConnect, defaultConnectInfo)
 import Lib
-import Snap.Core
-import Snap.Http.Server
+import Snap.Core (Snap, ifTop, route, writeBS)
+import Snap.Http.Server (quickHttpServe)
 
 main :: IO ()
 main = do
-  putStrLn "Init"
-  someFunc
+  redisConnection <- checkedConnect defaultConnectInfo
+  quickHttpServe $ webServer redisConnection
+
 -- TODO: Define the API for ingesting analytics events here - think of this file as linking
 -- http with the streams based backend
+webServer :: Connection -> Snap ()
+webServer redisConnection =
+  ifTop (writeBS "System is healthy!") <|>
+  route [("capture", analyticsEndpoint redisConnection)]
 
-WebServer :: Connection -> Snap ()
-WebServer redisConnection =
-  ifTop (writeBS "System is healthy!")
+analyticsEndpoint :: Connection -> Snap ()
+analyticsEndpoint redisConnection = do
+  writeBS "{\"ok\":true}"
